ba31c8f816b2b36fc643bf192be91e400247b069
DLHTX <35157091+DLHTX@users.noreply.github.com>
2023-08-09 12:17:56 +0800

    update nextui

--- src/app/components/layout_client.tsx
+++ src/app/components/layout_client.tsx
@@ -1,11 +1,17 @@
 'use client';
-import { AppProvider, HeaderStore, RootStore } from '@dappworks/kit';
+
+import { AppProvider, HeaderStore, RootStore, rootStore } from '@dappworks/kit';
 import { observer } from 'mobx-react-lite';
 import '../../store';
 import "../../styles/globals.css"
+import { signIn } from 'next-auth/react';
 
 export const Layout = observer(() => {
   const headerStore = RootStore.Get(HeaderStore);
+  rootStore.events.on('next.signIn.github', () => {
+    signIn('github');
+  });
+  
   return (
     <AppProvider>
       <headerStore.Header />
--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { AsyncStorage, DefaultEventMap, DevInspectorPlugin, DevTool, HeaderStore, StoragePlugin, ThemePlugin, WalletConnectButton, rootStore } from '@dappworks/kit';
 import { signIn } from 'next-auth/react';
 
@@ -6,9 +7,9 @@ rootStore.addStores([
   new StoragePlugin(),
   new AsyncStorage(),
   new HeaderStore({
-    UserNav: () => {
-      return <WalletConnectButton className='ml-auto'/>;
-    },
+    // UserNav: (props) => {
+    //   return <WalletConnectButton className={...props.className}></WalletConnectButton>;
+    // },
   }),
   // new SlotPlugin(),
   // new JSONViewPlugin(),
@@ -20,6 +21,3 @@ if (process.env.NODE_ENV == 'development') {
   rootStore.events.on('*', console.log);
 }
 
-rootStore.events.on('next.signIn.github', () => {
-  signIn('github');
-});
