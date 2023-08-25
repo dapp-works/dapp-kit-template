37c6196c1842e0747beba0ccc1c579e14e15c945
DLHTX <35157091+DLHTX@users.noreply.github.com>
2023-08-25 09:40:51 +0800

    update template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,45 @@
 'use client';
+import { JSONMetricsView, JSONSchemaTableState, JSONTable, PaginationState } from "@dappworks/jsonview"
 
-import {Button} from '@nextui-org/react'; 
+const table = new JSONSchemaTableState({
+  columns: [{
+    key: "nonce",
+    label: "NONCE",
+  }, {
+    key: "destination",
+    label: "DESTINATION"
+  }],
+  dataSource: new Array(100).fill(0).map((_, i) => ({
+    nonce: i,
+    destination: '0x' + Math.floor(Math.random() * 100
+    ).toString(16),
+  })),
+  rowKey: "id",
+  pagination: new PaginationState({
+    page: 1,
+    limit: 8
+  }),
+})
 
 export default function HomePage() {
   return (
-    <main className="">
-      <Button>123</Button>
+    <main className="p-10">
+      <JSONMetricsView data={[{
+        type: 'KPICard',
+        title: 'Data Messages',
+        description: 'Total number of messages received from all devices',
+        data: new Array(30).fill(0).map((_, i) => ({
+          date: new Date(2021, 0, i + 1).toISOString(),
+          SemiAnalysis: Math.floor(Math.random() * 100),
+          'The Pragmatic Engineer': Math.floor(Math.random() * 100),
+        })),
+        index: 'date',
+        categories: ["SemiAnalysis", "The Pragmatic Engineer"],
+        metricTitle: 'Total Events',
+        metric: 124,
+        chartType: 'area',
+      }]} />
+      <JSONTable jsonstate={{ table }} className='mt-2'></JSONTable>
     </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
   content: [
     './src/**/*.{js,ts,jsx,tsx}',
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@tremor/**/*.{js,ts,jsx,tsx}',
     './node_modules/@dappworks/kit/**/*.{js,ts,jsx,tsx}', 
   ],
   darkMode: 'class',
@@ -113,20 +114,44 @@ module.exports = {
       },
       boxShadow: {
         // light
-        "tremor-input": "0 1px 2px 0 rgb(0 0 0 / 0.05)",
-        "tremor-card": "0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)",
-        "tremor-dropdown": "0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)",
+        'tremor-input': '0 1px 2px 0 rgb(0 0 0 / 0.05)',
+        'tremor-card': '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)',
+        'tremor-dropdown': '0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)',
         // dark
-        "dark-tremor-input": "0 1px 2px 0 rgb(0 0 0 / 0.05)",
-        "dark-tremor-card": "0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)",
-        "dark-tremor-dropdown": "0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)",
+        'dark-tremor-input': '0 1px 2px 0 rgb(0 0 0 / 0.05)',
+        'dark-tremor-card': '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)',
+        'dark-tremor-dropdown': '0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)',
+      },
+      fontSize: {
+        'tremor-label': ['0.75rem'],
+        'tremor-default': ['0.875rem', { lineHeight: '1.25rem' }],
+        'tremor-title': ['1.125rem', { lineHeight: '1.75rem' }],
+        'tremor-metric': ['1.875rem', { lineHeight: '2.25rem' }],
+      },
+      borderRadius: {
+        'tremor-small': '0.375rem',
+        'tremor-default': '0.5rem',
+        'tremor-full': '9999px',
+        lg: `var(--radius)`,
+        md: `calc(var(--radius) - 2px)`,
+        sm: 'calc(var(--radius) - 4px)',
       },
       // fontFamily: {
       //   sans: ['Inter', 'var(--font-sans)', ...fontFamily.sans],
       // },
+      keyframes: {
+        'accordion-down': {
+          from: { height: 0 },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: 0 },
+        },
+      },
       animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
     },
   },
