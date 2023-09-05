19ec64b9ae8456f8fc231607d89a913be09777a7
DLHTX <949729789@qq.com>
2023-09-05 16:03:29 +0800

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,47 +4,49 @@ import { LayoutClient } from "./components/layout_client";
 import { TRPCProvider } from "./components/trpc_provider";
 import Head from "next/head";
 
-async function getData() {
-  const res = await fetch(`https://dappkit-async-api.deno.dev/project/${process.env.NEXT_PUBLIC_PROJECT_ID}`, {
-    cache: 'no-store',
-  })
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
-  return res.json()
-}
+// export const metadata = {
+//   manifest: "/manifest.json",
+//   themeColor: "#ffffff",
+// };
+
+// async function getData() {
+//   const res = await fetch(`https://dappkit-async-api.deno.dev/project/${process.env.NEXT_PUBLIC_PROJECT_ID}`, {
+//     cache: 'no-store',
+//   })
+//   if (!res.ok) {
+//     throw new Error('Failed to fetch data')
+//   }
+//   return res.json()
+// }
 
 
 //https://nextjs.org/docs/app/api-reference/functions/generate-metadata#title
 export async function generateMetadata(): Promise<Metadata> {
-  const { project } = await getData()
+  // const { project } = await getData()
   return {
-    title: project?.title,
-    description: project?.description,
+    // title: project?.title,
+    // description: project?.description,
+    manifest: "/manifest.json",
+    themeColor: "#ffffff",
   }
 }
 
 export default async function Layout(props: { children: React.ReactNode }) {
-  const { project } = await getData()
+  // const { project } = await getData()
   return (
     <TRPCProvider>
       <html lang="en">
-        <Head>
-          <link rel="manifest" href="/manifest.json" />
-          <link rel="apple-touch-icon" href="/icon.png"></link>
-          <meta name="theme-color" content="#fff" />
-        </Head>
         <body>
           <LayoutClient>
             {props.children}
           </LayoutClient>
           {/* <TRPCReactProvider headers={headers()}>{props.children}</TRPCReactProvider> */}
         </body>
-        {
+        {/* {
           project?.script && <Script>
             {`${project.script} `}
           </Script>
-        }
+        } */}
       </html>
     </TRPCProvider>
   );
