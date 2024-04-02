f962b8b485e0738fa7f3fc5a1c8ab333b6adabd6
uu-z <ncysatnaf@gmail.com>
2024-04-02 17:47:52 +0800

    clean up dependency

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,7 @@
 'use client';
 
 import { rootStore, helper } from '@dappworks/kit';
-import { DevInspectorPlugin } from '@dappworks/kit/inspector';
 import { ToastPlugin } from '@dappworks/kit/plugins';
-import { DevTool } from '@dappworks/kit/dev';
 import { signIn } from 'next-auth/react';
 import { enableStaticRendering } from 'mobx-react-lite';
 import { useEffect } from 'react';
@@ -13,7 +11,9 @@ enableStaticRendering(typeof window === 'undefined');
 export const initStore = () => {
   useEffect(() => {
     if (process.env.NODE_ENV == 'development') {
-      rootStore.addStores([new DevTool(), new DevInspectorPlugin()]);
+      Promise.all([import("@dappworks/kit/dev"), import('@dappworks/kit/inspector')]).then(([{ DevTool }, { DevInspectorPlugin }]) => {
+        rootStore.addStores([new DevTool(), new DevInspectorPlugin()]);
+      });
     }
 
     rootStore.addStores([new ToastPlugin()]);
